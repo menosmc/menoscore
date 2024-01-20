895e4faa0ae2aca4504d33ad735912408093ce81
frikandelworst <kyanvde@gmail.com>
2024-01-20 20:35:27 +0100

    Initialised a basic plugin.

---
+++ src/main/java/com/menosmc/menoscore/Menoscore.java
@@ -0,0 +1,17 @@
+package com.menosmc.menoscore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Menoscore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
